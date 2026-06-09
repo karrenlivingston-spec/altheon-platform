cbae45bc526ef4c52e55fa77e6563caf304085ce
Karren Livingston <karrlivingston@gmail.com>
2026-06-09 10:44:36 -0400

    fix: add TURN servers for cross-network WebRTC peer connection

--- frontend/src/components/virtual-visit/VirtualVisitRoom.tsx
+++ frontend/src/components/virtual-visit/VirtualVisitRoom.tsx
@@ -194,7 +194,25 @@ export default function VirtualVisitRoom({ roomId }: VirtualVisitRoomProps) {
           trickle: true,
           stream,
           config: {
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+            iceServers: [
+              { urls: "stun:stun.l.google.com:19302" },
+              { urls: "stun:stun1.l.google.com:19302" },
+              {
+                urls: "turn:openrelay.metered.ca:80",
+                username: "openrelayproject",
+                credential: "openrelayproject",
+              },
+              {
+                urls: "turn:openrelay.metered.ca:443",
+                username: "openrelayproject",
+                credential: "openrelayproject",
+              },
+              {
+                urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                username: "openrelayproject",
+                credential: "openrelayproject",
+              },
+            ],
           },
         });
 
